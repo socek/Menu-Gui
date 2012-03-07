67cef5652dcc59307b2879fcd5e856a5d6b0b40c
Dominik Socek Długajczyk <dominik.dlugajczyk@codepill.com>
2012-03-07 11:29:08 +0100

    Moved py files.

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+from setuptools import setup, find_packages
+import sys
+
+if __name__ == '__main__':
+    setup(name='Menu-Gui',
+        version='0.3',
+        description="",
+        author='Dominik "Socek" Długajczyk',
+        author_email='msocek@gmail.com',
+        license='?',
+        packages=find_packages(),
+        install_requires=[],
+    )
